f5709016f6aea7aa85675064278207b8e1845ff3
parichit21 <parichitkukreti@gmail.com>
2025-06-17 00:27:35 +0530

    added Login test

--- src/main/java/com.makemytrip.pages/LoginPage.java
+++ src/main/java/com.makemytrip.pages/LoginPage.java
@@ -1,4 +1,43 @@
 package com.makemytrip.pages;
 
-public class LoginPage {
+import com.makemytrip.base.baseTest;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginPage extends baseTest {
+    private static final Logger log = LoggerFactory.getLogger(LoginPage.class);
+    @FindBy(xpath = "//p[text()=\"Login or Create Account\"]")
+    WebElement login;
+
+    @FindBy(xpath = "//input[@placeholder=\"Enter Mobile Number\"]")
+WebElement Entermobile;
+
+    @FindBy(xpath = "//span[text()=\"Continue\"]")
+    WebElement continuebtn;
+
+    public LoginPage(){
+        super();
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login(){
+        login.click();
+    }
+
+    public void EnterMobile(){
+        Entermobile.sendKeys("9999332133");
+    }
+
+    public void ClickContinue(){
+        continuebtn.click();
+    }
+
+
+
+
+
+
 }
--- src/main/java/com.makemytrip.util/com.makemytrip.base/TestUtil.java
+++ src/main/java/com.makemytrip.util/com.makemytrip.base/TestUtil.java
@@ -2,8 +2,15 @@ package com.makemytrip.util.com.makemytrip.base;
 
 import com.makemytrip.base.baseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.io.FileHandler;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class TestUtil extends baseTest {
 
@@ -28,6 +35,18 @@ public static By customXpath(String xpath,String parm){
         String rawpath = xpath.replaceAll("%replace%",parm);
         return By.xpath(rawpath);
 }
+
+public static String getScreenShort() throws IOException {
+
+        String currentDate = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+
+        File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String destination = System.getProperty("user.dir")+"\\src\\main\\resources\\ScreenShort\\"+currentDate+".png";
+        FileHandler.copy(source, new File(destination));
+        return destination;
+    }
+
+
 
 
 }
